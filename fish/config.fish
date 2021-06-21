e2c926bfdf2a0acd842045952fafe6d54667c345
Lautaro Bachmann <laurigbach@gmail.com>
2021-06-21 09:09:43 -0300

    Fixed mistake

---
+++ fish/config.fish
@@ -0,0 +1,88 @@
+#neofetch
+set fish_greeting # Supresses fish's intro message
+
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+
+set -x EDITOR "nvim"
+set -x SUDO_ASKPASS "$HOME/.config/scripts/dmenupass sudo -A dmenu"
+set -x PATH "/home/lautarob/.config/scripts" $PATH
+#pyenv
+#set -x PATH "/home/lautarob/.pyenv/bin" $PATH
+#status --is-interactive; and . (pyenv init -|psub)
+#status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function to go to code folder
+function cdc 
+    set argument $argv .
+    cd ~/Documents/code/$argument[1]
+end
+# Function to change screen brightness
+function dbright
+    xrandr --output HDMI-0 --brightness $argv
+end
+
+# Activate python venv
+alias venv='source venv/bin/activate.fish'
+
+# navigation
+alias ..='cd ..' 
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+alias home='cd ~'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+# the terminal rickroll
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+
---
+++ nvim/init.vim
@@ -0,0 +1,57 @@
+set number 
+set mouse=a 
+set numberwidth=2
+set clipboard=unnamed
+syntax enable
+set showcmd
+set ruler
+set encoding=utf-8
+set showmatch
+set sw=2
+set relativenumber
+set laststatus=2
+:filetype on
+call plug#begin('~/.nvim/plugged')
+Plug 'joshdick/onedark.vim'
+Plug 'preservim/nerdtree' |
+            \ Plug 'Xuyuanp/nerdtree-git-plugin' |
+            \ Plug 'ryanoasis/vim-devicons'
+
+Plug 'christoomey/vim-tmux-navigator'
+Plug 'mattn/emmet-vim'
+Plug 'tpope/vim-fugitive'
+Plug 'sirver/ultisnips'
+    let g:UltiSnipsExpandTrigger = '<tab>'
+    let g:UltiSnipsJumpForwardTrigger = '<tab>'
+    let g:UltiSnipsJumpBackwardTrigger = '<s-tab>'
+    let g:UltiSnipsSnippetDirectories = ['~/.nvim/plugged/vim-snippets/UltiSnips']
+Plug 'lervag/vimtex'
+    let g:tex_flavor='latex'
+    let g:vimtex_view_method='zathura'
+    let g:vimtex_quickfix_mode=0
+
+Plug 'KeitaNakamura/tex-conceal.vim'
+    set conceallevel=2
+    let g:tex_conceal='abdmg'
+    hi Conceal ctermbg=none
+
+Plug 'honza/vim-snippets'
+
+call plug#end()
+let mapleader = " "
+let NERDTreeQuitOnOpen=1
+
+nmap <Leader>nt :NERDTreeFind<CR>
+nmap <Leader>rp :w <bar> :! python %<CR>
+nmap <Leader>y "+y<CR>
+nmap <Leader>p "+p<CR>
+
+:nmap <Leader>cs :!sassc % %:r.css<CR>
+autocmd BufWritePost,FileWritePost *.scss :!sassc % %:r.css
+autocmd BufWritePost,FileWritePost *.mom :silent :!pdfmom -e % > %:r.pdf
+autocmd BufWritePost,FileWritePost *.ms :silent :!groff -e -ms % -T pdf > %:r.pdf
+autocmd BufRead,BufNewFile *.pmd setfiletype pandocmd
+autocmd BufWritePost,FileWritePost *.pmd :silent :!pandoc -f markdown -t latex % -o %:r.pdf
+autocmd BufWritePost,FileWritePost *.tex :silent :!pdflatex %
+hi clear Conceal
+colorscheme onedark
