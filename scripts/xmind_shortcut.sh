e63c4922c7e50a217d1b6f9523455cb6b8ae797c
Lautaro Bachmann <laurigbach@gmail.com>
2021-10-31 21:52:45 -0300

    Removed unused stuff

--- scripts/xmind_shortcut.sh
+++ scripts/xmind_shortcut.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 shortcuts=(space ctrl+a ctrl+c enter ctrl+shift+e ctrl+v Escape space ctrl+a ctrl+x enter)
-time=(0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1)
 for i in "${!shortcuts[@]}"; do
     xdotool key "${shortcuts[i]}"
-    sleep "${time[i]}"
+    sleep 0.1
 done
 
