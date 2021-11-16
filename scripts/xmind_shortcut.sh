7e1b315203a5e4b7b7bd4092cee9bdced382a156
Lautaro Bachmann <laurigbach@gmail.com>
2021-11-15 21:35:07 -0300

    Added array to control how much time each action waits after executing

--- scripts/xmind_shortcut.sh
+++ scripts/xmind_shortcut.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 shortcuts=(space ctrl+a ctrl+c enter ctrl+shift+e ctrl+v Escape space ctrl+a ctrl+x enter)
+time=(0.1 0.1 0.1 0.1 0.1 0.1 0.15 0.15 0.15 0.15)
 for i in "${!shortcuts[@]}"; do
     xdotool key "${shortcuts[i]}"
-    sleep 0.1
+    sleep "${time[i]}"
 done
 
