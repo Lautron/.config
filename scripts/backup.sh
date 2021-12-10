4d7baf9ce13aeddd7c16bfe04262dfe99a4b8d74
Lautaro Bachmann <laurigbach@gmail.com>
2021-12-09 23:00:10 -0300

    Removed empty lines

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+rsync -iPa -v -e "ssh -i $HOME/.ssh/id_rsa" --exclude={'venv','node_modules'} ~/Documents/ lautarob-netbook:~/Documents/
 
-rsync -iPa -v --exclude={'venv','node_modules'} ~/Documents/ lautarob-netbook:~/Documents/
