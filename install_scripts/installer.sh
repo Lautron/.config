66b246e241855f496bb679767020b9037ae2d901
Lautaro Bachmann <laurigbach@gmail.com>
2023-01-24 19:25:50 -0300

    Enhanced logging

--- install_scripts/installer.sh
+++ install_scripts/installer.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-COLOR='\033[0;32m'
-NOCOLOR='\033[0m'
 pip_packages="neovim auto-editor termdown togglCli"
+success="DONE\n"
 
-function log-status() {
-  echo -e "${COLOR}$1${NOCOLOR}"
-}
-
-log-status "Installing paru..."
+source log-status "Installing paru..."
 [ -x "$(command -v paru)" ] || source install_paru.sh
+source log-status "$success"
 
-log-status "Update mirrors..."
+source log-status "Update mirrors..."
 paru -Sy --needed --noconfirm reflector
 sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+source log-status "$success"
 
-log-status "Installing packages.."
+source log-status "Installing packages.."
 paru -Sy --needed --noconfirm - < pacman-list.txt
+source log-status "$success"
 
-log-status "Setting git credentials..."
+source log-status "Setting git credentials..."
 git config --global user.email "laurigbach@gmail.com"
 git config --global user.name "Lautaro Bachmann"
+source log-status "$success"
 
-log-status "Setting up xmonad..."
+source log-status "Setting up xmonad..."
 # create .xmonad if it does not exist
 [  ~/.xmonad ] || mkdir ~/.xmonad
 # create hard link for xmonad.hs
 [ -f ~/.xmonad/xmonad.hs ] || ln ~/.config/xmonad/xmonad.hs ~/.xmonad/ 
 # compile xmonad
 xmonad --recompile
+source log-status "$success"
 
 # enable ly display manager
-log-status "Enabling display manager..."
+source log-status "Enabling display manager..."
 systemctl is-active --quiet ly.service || sudo systemctl enable ly.service 
+source log-status "$success"
 
 # set up fish
-log-status "Setting up fish..."
+source log-status "Setting up fish..."
 [ ${SHELL: -4} == "fish" ] || chsh -s $(which fish)
+source log-status "$success"
 
 # Ensure pip is installed
-log-status "Installing python packages..."
+source log-status "Installing python packages..."
 [ -f ~/.local/bin/pip3 ] || python3 -m ensurepip && pip install $pip_packages
+source log-status "$success"
 
--- install_scripts/setup_ssh.sh
+++ install_scripts/setup_ssh.sh
@@ -1,11 +1,26 @@
+success="DONE\n"
+
 # Set up ssh key
+source log-status "Creating ssh key..."
 ssh-keygen -t ed25519 -C "laurigbach@gmail.com"
+source log-status "$success"
+
 exec ssh-agent bash &
+
+source log-status "Adding ssh key..."
 ssh-add ~/.ssh/id_ed25519
+source log-status "$success"
+
+source log-status "Check if xclip is installed..."
 which xclip || sudo pacman -S xclip
+source log-status "$success"
+
+source log-status "Copying ssh public key to clipboard..."
 xclip -selection clipboard < ~/.ssh/id_ed25519.pub
-echo ""
-echo "Ssh public key copied to clipboard"
+source log-status "$success"
+
+source log-status "Enabling ssh service..."
 sudo systemctl start sshd 
 sudo systemctl enable sshd
+source log-status "$success"
 
