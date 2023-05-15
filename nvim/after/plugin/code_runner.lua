891e3c14a42a5ce0dffb68a68033e3880c79fb97
Lautaro Bachmann <laurigbach@gmail.com>
2023-05-15 10:47:07 -0300

    Add project to code_runner

--- nvim/after/plugin/code_runner.lua
+++ nvim/after/plugin/code_runner.lua
@@ -3,6 +3,14 @@ require('code_runner').setup({
     filetype = {
         python = "venv && python3 -u",
     },
+    project = {
+    ["~/Documents/Facultad/año3/PdP/lab/lab2/grupo09_lab02_2023"] = {
+       name = "PdP java",
+       description = "Named entity project",
+       command = "make"
+      }
+    }
 })
 
 vim.keymap.set('n', '<leader>rc', ':RunCode<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
