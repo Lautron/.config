ce46c18f5438de32306d60907d06ca419cd548b1
Lautaro Bachmann <laurigbach@gmail.com>
2023-09-15 09:19:42 -0300

    Add new scripts and disable ttask

---
+++ scripts/comedor.sh
@@ -0,0 +1,4 @@
+code=04756E3C28288F7
+echo $code | xclip -selection clipboard
+notify-send "Usuario comedor copiado al portapapeles"
+$BROWSER https://comedor.unc.edu.ar/reserva/aplicacion.php?ah=st64dcad173ef055.52063849&ai=migestion%7C%7C3616
