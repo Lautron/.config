b548c12801420e1c10ec6c2c89e9dc1c9a34ba3e
Lautaro Bachmann <laurigbach@gmail.com>
2023-10-19 08:51:44 -0300

    Add script to calculate rent

---
+++ scripts/alquiler.sh
@@ -0,0 +1,5 @@
+monto=22632
+echo $monto | xclip -selection clipboard
+notify-send "Monto alquiler copiado a portapapeles"
+$BROWSER https://www.aguascordobesas.com.ar/espacioClientes/seccion/gestionDeuda/consulta/528149
+
