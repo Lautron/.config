69d8acde320f5ddd5bdf2296f2798e433943ccd0
Lautaro Bachmann <laurigbach@gmail.com>
2024-10-02 20:57:20 -0300

    Stop usin xelatex (slow af)

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for file in "$@"; do
-  pandoc --pdf-engine=xelatex -M documentclass=scrartcl -M numbersections=true --toc --toc-depth=4 -f markdown -t latex "$file" -o "${file%.md}.pdf"
+  pandoc -M documentclass=scrartcl -M numbersections=true --toc --toc-depth=4 -f markdown -t latex "$file" -o "${file%.md}.pdf"
 done
