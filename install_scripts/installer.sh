604015f02e90df48a29cfe37199522868002442c
Lautaro Bachmann <laurigbach@gmail.com>
2026-05-20 13:57:30 -0300

    Update installer to: - Install pacman packages - Set up ly correctly - Set up zsh instead of fish

--- install_scripts/installer.sh
+++ install_scripts/installer.sh
@@ -12,6 +12,7 @@ sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 source log-status "$success"
 
 source log-status "Installing packages.."
+paru -Sy --needed --noconfirm - < packages.86_64
 paru -Sy --needed --noconfirm - < aur-pkgs.txt
 source log-status "$success"
 
@@ -24,19 +25,30 @@ source log-status "Setting up xmonad..."
 # create .xmonad if it does not exist
 [  $HOME/.xmonad ] || mkdir $HOME/.xmonad
 # create hard link for xmonad.hs
-[ -f $HOME/.xmonad/xmonad.hs ] || ln $HOME/.config/xmonad/xmonad.hs $HOME/.xmonad/ 
+[ -f $HOME/.xmonad/xmonad.hs ] || ln $HOME/.config/xmonad/xmonad.hs $HOME/.xmonad/
 # compile xmonad
 xmonad --recompile
 source log-status "$success"
 
-# enable ly display manager
+# enable ly display manager and configure TTY1
 source log-status "Enabling display manager..."
-systemctl is-active --quiet ly.service || sudo systemctl enable ly.service 
+systemctl is-active --quiet ly.service || sudo systemctl enable ly.service
+sudo systemctl enable ly@tty1.service
+sudo systemctl disable getty@tty1.service
 source log-status "$success"
 
 # set up fish
-source log-status "Setting up fish..."
-[ ${SHELL: -4} == "fish" ] || chsh -s $(which fish)
+source log-status "Setting up zsh..."
+[ ${SHELL: -4} == "zsh" ] || chsh -s $(which zsh)
+source log-status "$success"
+
+source log-status "Installing starship..."
+sudo pacman -S --needed --noconfirm starship
+source log-status "$success"
+
+source log-status "Linking zsh configs..."
+[ -f $HOME/.zshrc ] || ln -sf $HOME/.config/zsh/.zshrc $HOME/.zshrc
+[ -f $HOME/.zshenv ] || ln -sf $HOME/.config/zsh/.zshenv $HOME/.zshenv
 source log-status "$success"
 
 # Ensure pip is installed
